d9fb75a822c4dfff9e108668b559a0df10a0ff13
zibuyilenzima-oss <zibuyile.nzima@gmail.com>
2025-11-07 16:38:53 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import * as serviceWorkerRegistration from './serviceWorkerRegistration'; // ✅ now correctly placed
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
