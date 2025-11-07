af3f6b8e7a29abfc9fe4188eda3a6e91eb8cad32
zibuyilenzima-oss <zibuyile.nzima@gmail.com>
2025-11-07 15:42:54 +0200

    Create index.js

---
+++ src/src/index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+import * as serviceWorkerRegistration from './serviceWorkerRegistration'; // ✅ now correctly placed
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
+
+serviceWorkerRegistration.register(); // ✅ keep this call down here
+
+reportWebVitals();
